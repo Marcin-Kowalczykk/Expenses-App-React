213d8ddbd03c84628de063cd177067594eb3a838
Marcin  Kowalczyk <kowalczyk.marcin@gmail.com>
2021-06-12 14:17:34 +0200

    new folder for my components

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ExpenseItem = () => {
+    return (
+        <div>
+            <h1>lala</h1>
+            <p>lala</p>
+            <p>lala</p>
+        </div>
+    );
+}
+
+export default ExpenseItem;
