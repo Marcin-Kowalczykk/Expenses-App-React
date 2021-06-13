42b953e5ee1b7bf87f673800033d641a929dac4a
Marcin  Kowalczyk <kowalczyk.marcin@gmail.com>
2021-06-13 13:17:16 +0200

    update components to smaller components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React from "react";
 import "./App.css";
-import ExpenseItem from "./Components/ExpenseItem";
-import { expenses } from "./Components/ExpensesList";
+import { expensesList } from "./Components/ExpensesList";
+import NewExpensesList from "./Components/Expenses";
 
 function App() {
   return (
     <div className="App">
-      <h1>Expense</h1>
-      <ExpenseItem
-        title={expenses[0].title}
-        date={expenses[0].date}
-        amount={expenses[0].amount}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        date={expenses[1].date}
-        amount={expenses[1].amount}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        date={expenses[2].date}
-        amount={expenses[2].amount}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        date={expenses[3].date}
-        amount={expenses[3].amount}
-      />
+      <h1>Expense Manager</h1>
+      <NewExpensesList items={expensesList} />
     </div>
   );
 }
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,17 +1,11 @@
 import React from "react";
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = ({ title, date, amount }) => {
-  const month = date.toLocaleString("pl-PL", { month: "long" });
-  const day = date.toLocaleString("pl-PL", { day: "2-digit" });
-  const year = date.getFullYear();
   return (
     <div className="expense-item">
-      <section>
-        <p>{month}</p>
-        <p>{day}</p>
-        <p>{year}</p>
-      </section>
+      <ExpenseDate date = {date}/>
       <section className="expense-item__description">
         <h2>{title}</h2>
         <p className="expense-item__price">${amount}</p>
